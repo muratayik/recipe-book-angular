fb0694165d5b8055a22dafd5e321eb5d531e6eba
Murat Ayik <murat.ayik@siemens.com>
2023-11-29 17:05:49 +0300

    Add bootstrap and navbar to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { CategoryListComponent } from './category/category-list/category-list.co
 import { MealListComponent } from './meal/meal-list/meal-list.component';
 import { MealDetailComponent } from './meal/meal-detail/meal-detail.component';
 import { AppRoutesModule } from './app.routes';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { AppRoutesModule } from './app.routes';
     CategoryListComponent,
     MealListComponent,
     MealDetailComponent,
+    NavbarComponent,
   ],
   imports: [BrowserModule, AppRoutesModule],
   providers: [],
